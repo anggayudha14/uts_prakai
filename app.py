8a4ade7aa2c164f4f0649d39a28e821f248e1504
anggayudha14 <164121561+anggayudha14@users.noreply.github.com>
2025-05-03 09:36:02 +0700

    Add Procfile and runtime.txt for Railway deployment

--- app.py
+++ app.py
@@ -146,4 +146,5 @@ def download():
     return send_file('uploaded/hasil_analisis.csv', as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
